2f1b25a5a8fc0a22762442be316a39024b3ffc05
tay cavett <taycavett98@gmail.com>
2022-12-08 13:19:28 -0600

    added sql file updated main class to exclude DataSourceAutoConfig

--- src/main/java/cavett/springframework/recipeapp/RecipeAppApplication.java
+++ src/main/java/cavett/springframework/recipeapp/RecipeAppApplication.java
@@ -2,8 +2,9 @@ package cavett.springframework.recipeapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class RecipeAppApplication {
 
 	public static void main(String[] args) {
